5d3da29311af1b64ada04ea7e59a8385e434e445
Oloro Oghenerume Benedict <olorobenpeters@gmail.com>
2025-07-20 19:55:46 +0100

    gallery

--- src/components/gallerysection.jsx
+++ src/components/gallerysection.jsx
@@ -1,13 +1,13 @@
-import img1 from './../assets/gallery.png';
-import img2 from './../assets/gallery.png';
-import img3 from './../assets/gallery.png';
-import img4 from './../assets/gallery.png';
-import img5 from './../assets/gallery.png';
-import img6 from './../assets/gallery.png';
-import img7 from './../assets/gallery.png';
-import img8 from './../assets/gallery.png';
-import img9 from './../assets/gallery.png';
-import img10 from './../assets/gallery.png';
+import img1 from '../assets/gallery.png';
+import img2 from '../assets/gallery.png';
+import img3 from '../assets/gallery.png';
+import img4 from '../assets/gallery.png';
+import img5 from '../assets/gallery.png';
+import img6 from '../assets/gallery.png';
+import img7 from '../assets/gallery.png';
+import img8 from '../assets/gallery.png';
+import img9 from '../assets/gallery.png';
+import img10 from '../assets/gallery.png';
 
 export default function GallerySection() {
   const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
