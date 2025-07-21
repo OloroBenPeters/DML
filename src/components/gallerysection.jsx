cc29ceb6974fbb38f4ca6c0c2560d63e4ebe1f3c
Oloro Oghenerume Benedict <olorobenpeters@gmail.com>
2025-07-21 12:52:42 +0100

    new gallery

--- src/components/gallerysection.jsx
+++ src/components/gallerysection.jsx
@@ -1,13 +1,13 @@
-import img1 from './../assets/gallery.png';
-import img2 from './../assets/gallery.png';
-import img3 from './../assets/gallery.png';
-import img4 from './../assets/gallery.png';
-import img5 from './../assets/gallery.png';
-import img6 from './../assets/gallery.png';
-import img7 from './../assets/gallery.png';
-import img8 from './../assets/gallery.png';
-import img9 from './../assets/gallery.png';
-import img10 from './../assets/gallery.png';
+import img1 from './../assets/webdev.jpg';
+import img2 from './../assets/webdev.jpg';
+import img3 from './../assets/webdev.jpg';
+import img4 from './../assets/webdev.jpg';
+import img5 from './../assets/webdev.jpg';
+import img6 from './../assets/webdev.jpg';
+import img7 from './../assets/webdev.jpg';
+import img8 from './../assets/webdev.jpg';
+import img9 from './../assets/webdev.jpg';
+import img10 from './../assets/webdev.jpg';
 
 export default function GallerySection() {
   const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
